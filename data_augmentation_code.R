f6f716e05654ec5f5fd0601dc87e59bb6783902e
Corey Katz <ckatz@uci.edu>
2019-12-07 19:29:00 -0800

    code for data augmentation

---
+++ data_augmentation_code.R
@@ -0,0 +1,42 @@
+library(tidyverse)
+library(truncnorm)
+library(mvtnorm)
+dat <- read_csv("Data/breast-cancer-wisconsin.data", 
+                col_names = c("ID", "thick","uni_size",
+                              "uni_shape","adhesion",
+                              "single_size","nuclei",
+                              "chrom","nucleoli","mitoses",
+                              "class"))
+dat$nuclei <- as.numeric(dat$nuclei)
+dat$class <- as.factor(dat$class)
+X <- as.matrix(cbind(1,dat[,c(2:6,8:10)]))
+y <- dat$class
+
+generate_Z <- function(y,X,beta){
+  if(y == 4){
+    Z <- rtruncnorm(1,a=0,mean = X %*% beta,sd=1)
+  } 
+  if(y == 2){
+    Z <- rtruncnorm(1,b=0,mean = X %*% beta,sd=1)
+  }
+  return(Z)
+}
+
+n_samp <- 5000
+n_burnin <- 1000
+
+beta <- matrix(NA,nrow = n_samp, ncol = ncol(X))
+beta[1,] <- rmvnorm(1,rep(0,ncol(X)))
+Z <- unlist(lapply(1:nrow(X), function(k){generate_Z(y[k],X[k,],beta[1,])}))
+
+for (i in 2:n_samp){
+  beta_z <- lm(Z~X-1)$coeff
+  sigma <- solve(t(X) %*% X)
+  beta[i,] <- rmvnorm(1,mean=beta_z,sigma=sigma)
+  Z <- unlist(lapply(1:nrow(X), function(k){generate_Z(y[k],X[k,],beta[i,])}))
+}
+colMeans(beta[(n_burnin+1):n_samp,])
+
+probit_model <- glm(y~X-1,family = binomial(link = "probit"))
+summary(probit_model)
+
