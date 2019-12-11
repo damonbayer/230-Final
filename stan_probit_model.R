d95c7574caa8b1596c7fd92bca311e81df84112e
Corey Katz <ckatz@uci.edu>
2019-12-10 20:06:59 -0800

    updated stan

--- stan_probit_model.R
+++ stan_probit_model.R
@@ -42,13 +42,39 @@ set.seed(123456)
 options(mc.cores = parallel::detectCores())  ## local multicore CPUs
 dat = list(N=N, K=K, X=X,y=y)
 model.stan = stan_model(model_code=model_string)
-r = sampling(model.stan, dat, chains = 6, iter = 10000, warmup =2000, thin = 100, init = "0")
+r = sampling(model.stan, dat, chains = 6, iter = 10000, warmup =2000, init = "0")
 write_rds(x = r, path = "~/Documents/STATS 230/230-Final/stan_samples.rds")
+
+#load("~/Documents/STATS 230/230-Final/stan_samples.rds")
 summary(r)$summary
+samples <- as.matrix(r)[,-10]
 
 
-library(brms)
-fit <- brm(class ~ thick+uni_size+uni_shape+adhesion+single_size+chrom+nucleoli+mitoses, 
-           data = data, family = binomial(link = "probit"))
-summary(fit)
 
+samples %>% 
+  as_tibble() %>% 
+  pivot_longer(cols = everything()) %>% 
+  ggplot(aes(x = value, fill = name)) +
+  facet_wrap(. ~ name, scales = "free", labeller = label_parsed) +
+  geom_density() +
+  theme_bw() +
+  theme(legend.position="none") + 
+  ggtitle("Posterior Distributions")
+
+samples %>% 
+  as_tibble() %>% 
+  pivot_longer(cols = everything()) %>% 
+  group_by(name) %>% 
+  mutate(t = row_number()) %>% 
+  ggplot(aes(x = t, y = value, color = name)) +
+  facet_wrap(. ~ name, scales = "free", labeller = label_parsed) +
+  geom_line() +
+  theme_bw() +
+  theme(legend.position="none") +
+  ggtitle("Trace Plot")
+
+# library(brms)
+# fit <- brm(class ~ thick+uni_size+uni_shape+adhesion+single_size+chrom+nucleoli+mitoses, 
+#            data = data, family = binomial(link = "probit"))
+# summary(fit)
+# 
