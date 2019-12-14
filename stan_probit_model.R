4bbe657d64a493f6681c750c14a4232bf3d5c42e
Corey Katz <ckatz@uci.edu>
2019-12-13 16:38:46 -0800

    updated code

--- stan_probit_model.R
+++ stan_probit_model.R
@@ -42,7 +42,7 @@ set.seed(123456)
 options(mc.cores = parallel::detectCores())  ## local multicore CPUs
 dat = list(N=N, K=K, X=X,y=y)
 model.stan = stan_model(model_code=model_string)
-r = sampling(model.stan, dat, chains = 6, iter = 10000, warmup =2000, init = "0")
+r = sampling(model.stan, dat, chains = 1, iter = 5000, warmup =1000, init = "0")
 write_rds(x = r, path = "~/Documents/STATS 230/230-Final/stan_samples.rds")
 
 #load("~/Documents/STATS 230/230-Final/stan_samples.rds")
